d5ab87038e2a1e2dede9aa884222d0010ed31283
Nico Madysa <nico.madysa@tu-dresden.de>
2017-04-29 00:38:21 +0200

    Document Printer.

--- src/consumers/printer.rs
+++ src/consumers/printer.rs
@@ -2,15 +2,23 @@
 use scenarios::Scenario;
 use super::Consumer;
 
+/// The string pattern that gets replaced in `Printer::template`.
 const PATTERN: &'static str = "{}";
 
+/// A `Consumer` of `Scenario`s that prints their name to `stdout`.
 #[derive(Debug)]
 pub struct Printer<'template, 'terminator> {
+    /// A string in which `PATTERN` is replaced by the scenario name.
     template: &'template str,
+    /// A string printed after each template.
     terminator: &'terminator str,
 }
 
 impl<'template, 'terminator> Printer<'template, 'terminator> {
+    /// Creates a new `Printer` with default values.
+    ///
+    /// The default values are `PATTERN` (i.e. `"{}"`) for `template`
+    /// and a newline (i.e. `"\n"`) for `terminator`.
     pub fn new() -> Self {
         Printer {
             template: PATTERN,
@@ -44,6 +52,19 @@ impl<'template, 'terminator> Printer<'template, 'terminator> {
         self
     }
 
+    /// Applies the printer to a string.
+    ///
+    /// This inserts `s` in `template` and appends `terminator` to the
+    /// result.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// extern crate scenarios
+    /// use scenaros::consumers::Printer;
+    /// let p = Printer::new()
+    /// assert_eq!(p.format("hello world"), "hello world\n".to_owned());
+    /// ```
     pub fn format(&self, s: &str) -> String {
         let mut result = self.template.replace(PATTERN, s);
         result.push_str(self.terminator);
@@ -51,13 +72,14 @@ impl<'template, 'terminator> Printer<'template, 'terminator> {
     }
 }
 
-impl<'template, 'terminator> Default for Printer<'template, 'terminator> {
+impl<'a, 'b> Default for Printer<'a, 'b> {
     fn default() -> Self {
         Printer::new()
     }
 }
 
-impl<'template, 'terminator> Consumer for Printer<'template, 'terminator> {
+impl<'a, 'b> Consumer for Printer<'a, 'b> {
+    /// Prints formatted scenario names to `stdout`.
     fn consume(&self, scenario: &Scenario) {
         print!("{}", self.format(scenario.name()));
     }
