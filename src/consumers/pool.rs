ff8dcb41c50d553dd14dc8bc2b9457bd471e8fb3
Nico Madysa <nico.madysa@tu-dresden.de>
2017-06-23 22:12:01 +0200

    Rework the consumers::Pool struct.

--- src/consumers/pool.rs
+++ src/consumers/pool.rs
@@ -1,7 +1,7 @@
 
 use std::io;
 use std::error::Error as StdError;
-use std::process::{Child, ExitStatus};
+use std::process::{Child, Command, ExitStatus};
 use std::fmt::{self, Display};
 use std::collections::VecDeque;
 
@@ -30,12 +30,38 @@ impl From<usize> for JobCount {
 }
 
 
+/// Extension trait that is used to patch `ExitStatus`.
+trait IntoResult<T, E> {
+    /// Converts `self` into a given result type.
+    fn into_result(self) -> Result<T, E>;
+}
+
+impl IntoResult<(), Error> for ExitStatus {
+    /// Converts an `ExitStatus` into a `Result`.
+    ///
+    /// If the status indicates success, `Ok(())`` is returned.
+    /// Otherwise, `Err(Error::CommandFailed)` is returned.
+    fn into_result(self) -> Result<(), Error> {
+        if self.success() {
+            Ok(())
+        } else {
+            Err(Error::CommandFailed(self))
+        }
+    }
+}
+
+
 /// A pool of processes which can run concurrently.
 ///
-/// The pool is used by continously `add`ing newly-started processes to
-/// it. The `add()` call will block if the maximum number of concurrent
-/// processes has been reached. Once all processes have been submitted
-/// you should call `join()` to wait until they have all finished.
+/// The pool is used by continously `add`ing `std::process::Command`
+/// objects that are used to spawn new processes. The `add()` call will
+/// block if the maximum number of concurrent processes has been
+/// reached. Once all processes have been submitted, you should call
+/// `join()` to wait until they have all finished.
+///
+/// # Panics
+/// Dropping a `Pool` with some processes still queued causes a panic.
+/// To avoid this, always call `join` before dropping your `Pool`.
 pub struct Pool {
     /// The maximum number of concurrent processes.
     pub num_jobs: JobCount,
@@ -58,28 +84,32 @@ impl Pool {
 
     /// Adds a new process to the pool.
     ///
-    /// This blockingly adds `process` to the pool of waited-on
-    /// processes. If the pool is full, i.e. has reached `num_jobs`
-    /// elements, this call waits until another process has finished.
+    /// If the pool is full, this call blocks until a child process
+    /// is finished. Then, the a new child process executing `command`
+    /// is spawned and added to the pool.
     ///
     /// # Errors
-    /// If the pool is not full, it does not block and always succeeds.
+    /// If the pool is not full, this call does not block and always
+    /// succeeds.
     ///
     /// If a waited-on process fails, this call fails with
     /// `Error::CommandFailed`. A waited-on process fails e.g. by
     /// returning a non-zero exit status or by aborting through a
-    /// signal.
+    /// signal. The passed `command` gets spawned anyway.
     ///
-    /// Even if an error occurs, the passed process is added to the
-    /// queue in any case.
-    pub fn add(&mut self, process: Child) -> Result<(), Error> {
+    /// If an `std::io::Error` occurs, it is retuned. The passed
+    /// `Command` is *not* added in this case.
+    pub fn add(&mut self, mut command: Command) -> Result<(), Error> {
         let mut result = Ok(());
         // If the queue is full, block until one job is finished.
         if self.queue.len() == self.num_jobs.get() {
-            let oldest_job = self.queue.pop_front().expect("pop from empty queue");
-            result = Pool::wait_for_job(oldest_job);
+            // Abort the function if `find_finished()` fails.
+            // But keep the `result` if the child process failed.
+            result = self.remove_finished()?
+                .expect("queue empty")
+                .into_result();
         }
-        self.queue.push_back(process);
+        self.queue.push_back(command.spawn()?);
         result
     }
 
@@ -95,21 +125,60 @@ impl Pool {
     ///
     /// Even if the call fails, all processes are waited on.
     pub fn join(&mut self) -> Result<(), Error> {
-        let mut result = Ok(());
-        for job in self.queue.drain(..) {
-            result = result.and(Pool::wait_for_job(job));
+        let mut total_result = Ok(());
+        for mut job in self.queue.drain(..) {
+            let current_result = job.wait();
+            total_result = total_result.and_then(|_| current_result?.into_result());
         }
-        result
+        total_result
     }
 
-    /// Implementation of the waiting behavior of `join()` and `add()`.
-    fn wait_for_job(mut job: Child) -> Result<(), Error> {
-        let exit_status = job.wait()?;
-        if exit_status.success() {
-            Ok(())
-        } else {
-            Err(Error::CommandFailed(exit_status))
+    /// Finds the first finished child process in the queue.
+    ///
+    /// This sequentially tries to wait for all queued processes. If
+    /// a process turns out to have finished, it is immediately removed
+    /// from the queue and its `ExitStatus` is returned.
+    ///
+    /// If all processes are still running, `Ok(None)` is returned.
+    ///
+    /// # Errors
+    /// This call fails if waiting on any process fails. The failing
+    /// process may or may not be left in the queue.
+    fn remove_finished(&mut self) -> io::Result<Option<ExitStatus>> {
+        if self.queue.is_empty() {
+            return Ok(None);
         }
+        loop {
+            let maybe_index = self.position_of_finished()?;
+            if let Some(index) = maybe_index {
+                return self.queue
+                           .remove(index)
+                           .expect("index returned by `find_finished` invalid")
+                           .wait()
+                           .map(|status| Some(status));
+            }
+        }
+    }
+
+    /// Finds the first finished child process in the queue.
+    ///
+    /// This sequentially tries to wait for all queued processes. If
+    /// a process turns out to have finished, its index in the queue is
+    /// returned immediately.
+    ///
+    /// If all processes are still running, `Ok(None)` is returned.
+    ///
+    /// # Errors
+    /// This call fails if any call to `try_wait()` fails.
+    fn position_of_finished(&mut self) -> io::Result<Option<usize>> {
+        for (i, job) in self.queue.iter_mut().enumerate() {
+            // Return on error or if `job` is finished.
+            if job.try_wait()?.is_some() {
+                return Ok(Some(i));
+            }
+        }
+        // No job finished, report that.
+        Ok(None)
     }
 }
 
@@ -120,6 +189,13 @@ impl Default for Pool {
     }
 }
 
+impl Drop for Pool {
+    fn drop(&mut self) {
+        if !self.queue.is_empty() {
+            panic!("dropping a non-empty process pool");
+        }
+    }
+}
 
 /// Error type returned by `Pool`'s methods.
 #[derive(Debug)]
